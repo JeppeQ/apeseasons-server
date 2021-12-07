827b9fbd0a62c41ebe5920072d5e1d1a56b78b04
Jeppe <jeppeqin@gmail.com>
2021-12-07 14:19:04 +0100

    update graph id and whitelisted tokens

--- providers/thegraph.js
+++ providers/thegraph.js
@@ -2,7 +2,7 @@ const fetch = require('cross-fetch')
 const { ApolloClient, InMemoryCache, gql, HttpLink } = require("@apollo/client/core")
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://api.thegraph.com/subgraphs/id/QmaVFwcm77nxYiYVFywyvLAKrH13DPFLsSJjZPhg61fhoC', fetch }),
+  link: new HttpLink({ uri: 'https://api.thegraph.com/subgraphs/id/QmNMzLjJo6BJ7FR98ycfodUSVDXmRZn4heZ1GtU6TDsBFv', fetch }),
   cache: new InMemoryCache()
 });
 
